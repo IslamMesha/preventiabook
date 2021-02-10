8d4024abef6063968b23cdc0cfae3054d0c91b17
Islam Mesha <eng.islammesha@gmail.com>
2021-02-10 16:48:50 +0200

    Remove secrt key

--- preventiabook/settings.py
+++ preventiabook/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n_m*0t(bl^!ypie1n6v11cd%(s47&ys_(gy_ldw&kfat_4hf!6'
+SECRET_KEY = os.environ.get('SECRET_KEY', None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
